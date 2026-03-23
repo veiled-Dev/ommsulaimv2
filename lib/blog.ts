5cb6c16d0d9f99b3d4af6a9b2f99423202b26dfd
ummhaanii <ummaabdul23@gmail.com>
2026-03-23 20:40:32 +0200

    Support KV_REST_API_* env vars for Redis connection

--- lib/blog.ts
+++ lib/blog.ts
@@ -33,8 +33,8 @@ function getRedisClient(): Redis | null {
     return redisClient;
   }
 
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url = process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL;
+  const token = process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN;
 
   if (!url || !token) {
     redisClient = null;
